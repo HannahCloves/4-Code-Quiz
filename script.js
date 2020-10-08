0cc559a2d1099e93d983424aa1a01e76191747ea
Hannah C <hannah.cloves@outlook.com>
2020-10-08 15:57:16 +0100

    remove the JS element out of HTML and built it entirely into JS

--- script.js
+++ script.js
@@ -2,11 +2,11 @@
 
 //When the start button is clicked, it will display the next question and restart button
 //Will also hide the start button
-    function startQuiz(){
+
+
+ var startButton = document.getElementById("buttonStart");
+    startButton.addEventListener('click', function() { 
         var showQuiz = document.getElementById("beginQuiz"); 
         showQuiz.setAttribute("style", "display:block;");
-        var startButton = document.getElementById("buttonStart");
         startButton.style.display = "none";
-    }
-
-    
+    })
