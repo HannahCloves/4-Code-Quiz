eb62f43159858ae299ce1c905f2ef4713bbf6def
Hannah C <hannah.cloves@outlook.com>
2020-10-09 13:27:11 +0100

    renamed button ids for questions

--- script.js
+++ script.js
@@ -24,20 +24,24 @@ startButton.addEventListener('click', function () {
     }, 1000);
 })
 
+var answerOne = document.getElementById("optionOne");
+var answerTwo = document.getElementById("optionTwo");
+var answerThree = document.getElementById("optionThree");
+var answerFour = document.getElementById("optionFour");
 
 var questionsObj = [
     {
-        title: "Commonly used data types DO NOT include:",
+        question: "Commonly used data types DO NOT include:",
         choices: ["strings", "booleans", "alerts", "numbers"],
         answer: "alerts"
     },
     {
-        title: "The condition in an if / else statement is enclosed within ____.",
+        question: "The condition in an if / else statement is enclosed within ____.",
         choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
         answer: "parentheses"
     },
     {
-        title: "Arrays in JavaScript can be used to store ____.",
+        question: "Arrays in JavaScript can be used to store ____.",
         choices: [
             "numbers and strings",
             "other arrays",
@@ -47,15 +51,14 @@ var questionsObj = [
         answer: "all of the above"
     },
     {
-        title:
-            "String values must be enclosed within ____ when being assigned to variables.",
+        question:"String values must be enclosed within ____ when being assigned to variables.",
         choices: ["commas", "curly brackets", "quotes", "parentheses"],
         answer: "quotes"
     },
     {
-        title:
-            "A very useful tool used during development and debugging for printing content to the debugger is:",
-        choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        choices: [
+            "JavaScript", "terminal / bash", "for loops", "console.log"],
         answer: "console.log"
     }
 ];
